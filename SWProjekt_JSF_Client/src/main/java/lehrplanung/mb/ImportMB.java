6b1af79f3f3386a72401ee8f2e5969f151086da3
Niklas <Niklas@DESKTOP-8D8NA8P.fritz.box>
2021-12-07 16:09:06 +0100

    Anpassungen

--- SWProjekt_JSF_Client/src/main/java/lehrplanung/mb/ImportMB.java
+++ SWProjekt_JSF_Client/src/main/java/lehrplanung/mb/ImportMB.java
@@ -1,10 +1,42 @@
 package lehrplanung.mb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.mail.Part;
+
+import de.lehrplanung.planung.entity.SemesterTO;
+import de.lehrplanung.planung.usecase.ISemesterLaden;
 
 @Named("importMB")
 @RequestScoped
 public class ImportMB {
 
+	@Inject
+	ISemesterLaden semesterLadenFacade;
+	
+	private SemesterTO semesterTO;
+	List<SemesterTO> geladeneSemester = new ArrayList<>();
+	
+	private Part uploadedFile;
+	
+	ImportMB() {
+		geladeneSemester = semesterLadenFacade.semesterLaden();
+	}
+	
+	public void upload() {
+		
+	}
+
+	public Part getUploadedFile() {
+		return uploadedFile;
+	}
+
+	public void setUploadedFile(Part uploadedFile) {
+		this.uploadedFile = uploadedFile;
+	}
+	
 }
